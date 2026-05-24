7baf08ca8b7405e55b277f656ad67e135b74a413
SharkFishDeveloper <shahzebakhtart66@gmail.com>
2026-05-24 21:34:13 +0530

    b

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser())
 app.use(cors({
-  // origin:"http://localhost:5173",
-  origin:"https://mentor-match-seven.vercel.app",
+  origin:"http://localhost:5173",
+  // origin:"https://mentor-match-seven.vercel.app",
   credentials:true
 }))
 const server  = http.createServer(app);
